92599117661e1e24d02dc3869934988a41ccbfb8
Reka jajeindren <Reka jajeindren@REKA>
2024-12-05 23:47:54 +0530

    RentalAPIUpdate

--- Movie_Rental_Management/Database/AppDbContext.cs
+++ Movie_Rental_Management/Database/AppDbContext.cs
@@ -23,54 +23,7 @@ namespace Movie_Rental_Management.Database
         public DbSet<Payment> Payment { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
         public DbSet<Review> Reviews { get; set; }
-        public DbSet<ContactUs> ContactUs
-        {
-            get; set;
-
-            //protected override void OnModelCreating(ModelBuilder modelBuilder)
-            //{
-            //    base.OnModelCreating(modelBuilder);
-
-            //    // User -> UserRoles
-            //    modelBuilder.Entity<User>()
-            //        .HasMany<UserRole>(u => u.UserRoles)
-            //        .WithOne()
-            //        .HasForeignKey(ur => ur.UserId)
-            //        .OnDelete(DeleteBehavior.Cascade); 
-
-            //    // Role -> UserRoles
-            //    modelBuilder.Entity<Role>()
-            //        .HasMany<UserRole>(r => r.UserRoles)
-            //        .WithOne()
-            //        .HasForeignKey(ur => ur.RoleId)
-            //        .OnDelete(DeleteBehavior.Restrict);
-
-            //    //// One Genre has many Movies
-            //    //modelBuilder.Entity<Movie>()
-            //    //    .HasOne(m => m.Genre) 
-            //    //    .WithMany(g => g.Movies)
-            //    //    .HasForeignKey(m => m.GenreId) 
-            //    //    .OnDelete(DeleteBehavior.Restrict); 
-
-            //    ////One Director has many Movies
-            //    //modelBuilder.Entity<Movie>()
-            //    //    .HasOne(m => m.Director) 
-            //    //    .WithMany(d => d.Movies) 
-            //    //    .HasForeignKey(m => m.DirectorId) 
-            //    //    .OnDelete(DeleteBehavior.Restrict); 
-            //}
-
-
-
-
-
-
-
-
-
-
-
-        }
+        public DbSet<ContactUs> ContactUs { get; set; }
     }
 }
 
--- Movie_Rental_Management/obj/Debug/net8.0/Movie_Rental_Management.AssemblyInfo.cs
+++ Movie_Rental_Management/obj/Debug/net8.0/Movie_Rental_Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movie_Rental_Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b36973d2d9cc99fc68432c76d5901a594d48e332")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d409befcfbbf6e0beacab8c6ce7365a9ef6bb7c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movie_Rental_Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movie_Rental_Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
