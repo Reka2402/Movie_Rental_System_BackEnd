1a3c57d0092a93dfc50c6dfdd7c4e9be9b3ef8a4
Reka2402 <ut01336tic2024@gmail.com>
2024-12-07 16:31:43 +0530

    Rental Fix

--- Movie_Rental_Management/obj/Debug/net8.0/Movie_Rental_Management.AssemblyInfo.cs
+++ Movie_Rental_Management/obj/Debug/net8.0/Movie_Rental_Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movie_Rental_Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aff05f7502a82b2b36cd5f7250a351035aa6bcb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60cf8fc75fc7449b5afea31b5ab0af8ef3c9f995")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movie_Rental_Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movie_Rental_Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
