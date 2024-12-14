2cddbbfd6fd3f40e39eedc6a604baed35f722ed3
Reka Jajeindren <ut01336tic2024@gmail.com>
2024-12-14 12:30:39 +0530

    Update IRentalService.cs

--- Movie_Rental_Management/IService/IRentalService.cs
+++ Movie_Rental_Management/IService/IRentalService.cs
@@ -8,6 +8,7 @@ namespace Movie_Rental_Management.IService
     {
         Task<RentalResponseModel> AddRental(RentalrequestModel rentalrequestModel);
         Task<List<RentalResponseModel>> GetAllRentals();
+             Task ApproedRental(Guid id);
         Task<RentalResponseModel> GetById(Guid Id);
         Task<RentalResponseModel> GetByUserID(Guid UserId);
         Task<RentalResponseModel> UpdateRent(Guid Id, RentalrequestModel rentalrequestModel);
